b9a04eebfac5d6fe0799c04688cf88729ebd6dc4
airwindow <350609633@qq.com>
2015-02-06 13:49:22 -0500

    the api for facebook!

---
+++ facebook_info_api.py
@@ -0,0 +1,8 @@
+@app.route('/info/<company_name>')
+def get_posts(company_name):
+    #Returns the list of updates of a company.
+    TOKEN ='575731909230644|ZuJwTeYLANGBOsZFWPczcx8JDZo'
+    parameters = {'access_token': TOKEN}
+    response = requests.get('https://graph.facebook.com/'+ company_name + '/feed', params=parameters)
+    response_dict = response.json()
+    return jsonify(response_dict)
