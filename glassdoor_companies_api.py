06a6e1cd10ce27cec19e4547f87c4cda2fdaa711
Jane Li <jl4372@columbia.edu>
2015-02-06 16:15:03 -0500

    Added Glassdoor API Access

--- glassdoor_companies_api.py
+++ glassdoor_companies_api.py
@@ -1,7 +1,7 @@
 import requests
 
-url = "http://api.glassdoor.com/api/api.htm?t.p=29781&t.k=jQdxv7dRxPc&userip=0.0.0.0&useragent=Chrome&format=json&v=1&action=employers&q=Google"
-response = requests.get(url)
-response_dict = response.json()
+url = "http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=29781&t.k=jQdxv7dRxPc&action=employers&q=Google&userip=0.0.0.0&useragent=Chrome/%2F4.0"
+user_agent = {'User-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.111 Safari/537.36'}
+response  = requests.get(url, headers = user_agent).json()
 
-return 
+print response
